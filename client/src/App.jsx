8c6cd5d1347dfac7e9e97afa26027ab1cbf3748d
laxmanp090404 <laxman.p2022cse@sece.ac.in>
2024-09-15 22:39:30 +0530

    feat: initial vite client setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <div style={{backgroundColor:"red"}}>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+export default App
