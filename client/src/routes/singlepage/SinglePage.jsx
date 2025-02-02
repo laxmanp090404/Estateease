f9c22df6c5fc84fff101ead9baf31237bcf9b69e
laxmanp090404 <laxman.p2022cse@sece.ac.in>
2025-02-02 19:41:00 +0530

    feat : SinglePage designed

--- client/src/routes/singlepage/SinglePage.jsx
+++ client/src/routes/singlepage/SinglePage.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
-
+import './SinglePage.scss'
+import Slider from '../../components/Slider/Slider'
+import { singlePostData, userData } from '../../lib/dummydata'
 const SinglePage = () => {
   return (
-    <div>SinglePage</div>
+    <div className="singlepage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images}/>
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <img src="/pin.png" alt="" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">₹ {singlePostData.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">
+              {singlePostData.description}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper">
+
+        </div>
+      </div>
+    </div>
   )
 }
 
